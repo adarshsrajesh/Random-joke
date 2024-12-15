8f5a19c0427fe3e088e3b04d73e7856fa0c10934
adarshsrajesh <adarshsrajesh@gmail.com>
2024-12-15 22:49:32 +0530

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+if(localStorage.getItem("setup")&&localStorage.getItem("punchline")){
+  msg.innerHTML= `
+ 
+            <h4>${localStorage.getItem("setup")}</h4>
+       `
+  pline.innerHTML =`<h4> ${localStorage.getItem("punchline")} </h4>`
+
+}else{
+  alert("click the button for a jokeS")
+}
+
+
 const joke = async() => {
 
   const response = await fetch("https://official-joke-api.appspot.com/random_joke")
@@ -5,6 +17,16 @@ const joke = async() => {
   if(response.status==200){
     displayData = await response.json()
     console.log(displayData);
+    msg.innerHTML= `
+ 
+    <h4>${displayData.setup}</h4>
+`
+pline.innerHTML =`<h4> ${displayData.punchline} </h4>`
+  
+ 
+    localStorage.setItem("setup",displayData.setup)
+    localStorage.setItem("punchline",displayData.punchline)
+ 
     
 
   }else{
